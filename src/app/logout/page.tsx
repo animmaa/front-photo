3c72bc0d16c43425a81fb8ac01e9a00a5c023eb4
davy lebris <davylebris@gmail.com>
2023-11-30 10:46:28 +0100

    ajout des champs du formulaire

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,3 +1,46 @@
+import './logout.scss';
+
 export default function logout() {
-  return <div>page logout</div>;
+  return (
+    <div className="logout">
+      <form>
+        <label htmlFor="email">
+          Email
+          <input type="text" />
+        </label>
+        <label htmlFor="password">
+          Mot de passe
+          <input type="text" />
+        </label>
+        <label htmlFor="lastname">
+          Nom
+          <input type="text" />
+        </label>
+        <label htmlFor="firstname">
+          Prénom
+          <input type="text" />
+        </label>
+        <label htmlFor="phone">
+          Téléphone
+          <input type="text" />
+        </label>
+        <label htmlFor="address">
+          Adresse
+          <input type="text" />
+        </label>
+        <label htmlFor="postalcode">
+          Code postal
+          <input type="text" />
+        </label>
+        <label htmlFor="city">
+          Ville
+          <input type="text" />
+        </label>
+        <label htmlFor="country">
+          Pays
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
 }
