ae0356f50b0b5095038788239f6ee03621aaee5f
davy lebris <davylebris@gmail.com>
2023-11-30 11:28:14 +0100

    ajout fonctionnalité logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,45 +1,82 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+
 import './logout.scss';
 
+interface FormValues {
+  email: string;
+  password: string;
+  lastname: string;
+  firstname: string;
+  phone: string;
+  address: string;
+  postalcode: number;
+  city: string;
+  country: string;
+}
+
 export default function logout() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  console.log(process.env.NEXT_PUBLIC_REACT_APP_API_URL);
+  const onSubmit = async (values: any) => {
+      console.log(values)
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/user/logout`,
+        values
+      );
+      console.log(response.data.message)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="logout">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email">
           Email
-          <input type="text" />
+          <input id="email" {...register('email')} />
         </label>
         <label htmlFor="password">
           Mot de passe
-          <input type="text" />
+          <input id="password" {...register('password')} />
         </label>
         <label htmlFor="lastname">
           Nom
-          <input type="text" />
+          <input id="lastname" {...register('lastname')} />
         </label>
         <label htmlFor="firstname">
           Prénom
-          <input type="text" />
+          <input id="firstname" {...register('firstname')} />
         </label>
         <label htmlFor="phone">
           Téléphone
-          <input type="text" />
+          <input id="phone" {...register('phone')} />
         </label>
         <label htmlFor="address">
           Adresse
-          <input type="text" />
+          <input id="address" {...register('address')} />
         </label>
         <label htmlFor="postalcode">
           Code postal
-          <input type="text" />
+          <input id='postalcode' type='number' {...register('postalcode')} />
         </label>
         <label htmlFor="city">
           Ville
-          <input type="text" />
+          <input id='city' {...register('city')} />
         </label>
         <label htmlFor="country">
           Pays
-          <input type="text" />
+          <input id='country' {...register('country')} />
         </label>
+        <button type='submit'>Créer un compte</button>
       </form>
     </div>
   );
