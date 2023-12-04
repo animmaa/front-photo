a57e3d3b7be622395258a7614662d3a6a9ea52a2
davy lebris <davylebris@gmail.com>
2023-12-04 11:32:22 +0100

    ajout de la fonction de connexion

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,58 @@
+'use client'
+
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
 export default function login() {
-    return <div>
-        page login
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit = async (values: FormValues) => {
+      console.log(values)
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/user/login`,
+        values
+      );
+      toast("vous etes maintenant connecté");
+    } catch (error) {
+      toast("une erreur s'est produite");
+    }
+  };
+
+  return (
+    <div>
+      <ToastContainer />
+      <h1>Connexion</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">
+          <input
+            type="text"
+            placeholder="Email"
+            id="email"
+            {...register('email')}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="text"
+            placeholder="Mot de passe"
+            id="password"
+            {...register('password')}
+          />
+        </label>
+        <button type="submit">Connexion</button>
+      </form>
     </div>
+  );
 }
